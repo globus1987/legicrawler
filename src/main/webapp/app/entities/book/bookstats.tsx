ba61e317163e2e99af16c03aefe287ff9bd93264
arek1987 <arek@kimputing.com>
2023-04-18 14:16:45 +0200

    crawler

--- src/main/webapp/app/entities/book/bookstats.tsx
+++ src/main/webapp/app/entities/book/bookstats.tsx
@@ -1,51 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { DetailHeader, GoBack } from 'app/entities/ReusableComponents';
+import { GoBack } from 'app/entities/ReusableComponents';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { getEntities } from 'app/entities/statistics/statistics.reducer';
-import { Line } from 'react-chartjs-2';
 import Chart from 'app/entities/book/chart';
 import { useNavigate } from 'react-router-dom';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Row, Col } from 'reactstrap';
-import { format, startOfWeek, startOfMonth, subMonths } from 'date-fns';
+import { Col, Row } from 'reactstrap';
+import { format, subDays } from 'date-fns';
+import { ValidatedField } from 'react-jhipster';
 
 export const BookStats = () => {
   const dispatch = useAppDispatch();
   const statisticsList = useAppSelector(state => state.statistics.entities);
+  const statisticsLoading = useAppSelector(state => state.statistics.loading);
   const loading = useAppSelector(state => state.statistics.loading);
   const navigate = useNavigate();
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [selectedFilter, setSelectedFilter] = useState('day');
+  const [selectedFilter, setSelectedFilter] = useState('');
+  const [chartData, setChartData] = useState();
 
   const toggleDropdown = () => setDropdownOpen(prevState => !prevState);
+
+  useEffect(() => {
+    dispatch(getEntities({}));
+  }, []);
+
+  useEffect(() => {
+    if (statisticsList.length > 0) setChartData(statisticsList);
+  }, [statisticsLoading]);
 
   const handleFilterChange = filter => {
     setSelectedFilter(filter);
   };
-  let chartData = statisticsList;
   useEffect(() => {
-    chartData = getFilteredData();
+    setChartData(getFilteredData());
   }, [selectedFilter]);
 
   const getFilteredData = () => {
     let filteredData = statisticsList;
 
-    if (selectedFilter === 'lastWeek') {
-      const weekStart = format(subWeeks(new Date(), 1), 'yyyy-MM-dd');
-      return statisticsList.filter(data => data.added >= weekStart);
-    } else if (selectedFilter === 'lastTwoWeeks') {
-      const weekStart = format(subWeeks(new Date(), 2), 'yyyy-MM-dd');
-      return statisticsList.filter(data => data.added >= weekStart);
-    } else if (selectedFilter === 'lastMonth') {
-      const monthStart = format(subMonths(new Date(), 1), 'yyyy-MM-dd');
-      return statisticsList.filter(data => data.added >= monthStart);
+    if (selectedFilter.length > 0) {
+      const start = format(subDays(new Date(), Number(selectedFilter) - 1), 'yyyy-MM-dd');
+      return statisticsList.filter(data => data.added >= start);
     }
 
     return filteredData;
   };
 
-  useEffect(() => {
-    dispatch(getEntities({}));
-  }, []);
   const handleBarClick = (label: string, value: number) => {
     window.localStorage.clear();
     window.localStorage.setItem('filter.added', new Date(label).toISOString());
@@ -56,17 +56,14 @@ export const BookStats = () => {
     <div>
       <Row>
         <Col>
-          <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
-            <DropdownToggle caret>{selectedFilter}</DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem onClick={() => handleFilterChange('last-week')}>Last Week</DropdownItem>
-              <DropdownItem onClick={() => handleFilterChange('last-two-weeks')}>Last Two Weeks</DropdownItem>
-              <DropdownItem onClick={() => handleFilterChange('last-month')}>Last Month</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </Col>
-        <Col>
-          <Chart chartData={chartData} chartType="day" onBarClick={handleBarClick} color="#FFA552" />
+          <ValidatedField
+            type="text"
+            name="filter"
+            label="Nr of days"
+            value={selectedFilter}
+            onChange={e => handleFilterChange(e.target.value)}
+          />
+          {chartData && <Chart chartData={chartData} chartType="day" onBarClick={handleBarClick} color="#FFA552" />}
         </Col>
         <Col>
           <Chart chartData={statisticsList} chartType="week" onBarClick={handleBarClick} color="#BA5624" />
@@ -81,6 +78,3 @@ export const BookStats = () => {
 };
 
 export default BookStats;
-function subWeeks(arg0: Date, arg1: number): any {
-  throw new Error('Function not implemented.');
-}
